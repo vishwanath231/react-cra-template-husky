7262f62b5ff615f977ee175c1bd7094d7dd83025
vishwanath231 <vishwanatharuchunan@gmail.com>
2025-04-21 17:45:22 +0530

    react cra template

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [users, setUsers] = React.useState([]);
-  console.log(users);
   return (
     <div className="App">
       <header className="App-header">
